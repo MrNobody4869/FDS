1c886364d5eb1fa5ebde6b448983cc06bf78aee9
MrNobody4869 <foxlucifer2@Gmail.com>
2024-12-12 23:38:13 +0530

    7. Pinnacle Club.cpp

--- 7. Pinnacle Club.cpp
+++ 7. Pinnacle Club.cpp
@@ -2,79 +2,163 @@
 #include<string>
 using namespace std;
 
-class Node {
+class node
+{
 public:
     int PRN;
     string Name;
-    Node* next;
+    node *next;
 };
 
-class SLL {
-private:
-    Node* head;
-    Node* last;
-    int value, op;
+class SLL
+{
+    node *head, *last;
+    int count;
 
 public:
-    SLL() {
-        head = NULL;
-        last = NULL;
+    SLL()
+    {
+        head = nullptr;
+        last = nullptr;
+        count=0;
     }
 
     void create();
     void display();
+    void search();
 };
 
-void SLL::create() {
-    Node* temp;
-    do {
-        if (head == NULL) {
-          
-            temp = new Node;
-            cout << "\n Enter PRN And Name Of President: ";
+void SLL::create()
+{
+    node *temp, *temp1;
+    int op;
+    do
+    {
+
+        if (head == nullptr)
+        {
+
+            temp = new node;
+            cout << "\n Enter PRN and Name of President: "<<endl;
             cin >> temp->PRN >> temp->Name;
-            temp->next = NULL;
+            temp->next = nullptr;
             head = temp;
 
-            
-            last = new Node;
-            cout << "\n Enter PRN And Name Of Secretary: ";
-            cin >> last->PRN >> last->Name;
-            last->next = NULL;
-            head->next = last;
-        }
-        else {
-            
-            temp = new Node;
-            cout << "\n Enter PRN And Name For Member: ";
+
+            temp = new node;
+            cout << "\n Enter PRN and Name of Secretary: "<<endl;
             cin >> temp->PRN >> temp->Name;
-            temp->next = NULL;
+            temp->next = nullptr;
+            last = temp;
 
-
-            temp->next = last;
-            head->next->next = temp;
-            head->next = temp;
+            head->next = last;
+            count+=2;
         }
-        cout << "\n Do you want to add new member (1/0): ";
+        else
+        {
+            temp1 = head;
+            while (temp1->next != last)
+            {
+                temp1 = temp1->next;
+            }
+            temp = new node;
+            cout << "\n Enter PRN and Name for Member: "<<endl;
+            cin >> temp->PRN >> temp->Name;
+            temp->next = last;
+            temp1->next = temp;
+            count++;
+        }
+
+        cout << "\n Do you want to add a new member? (1 for Yes / 0 for No): "<<endl;
         cin >> op;
     } while (op == 1);
+    count++;
 }
 
-void SLL::display() {
-    Node* temp;
-    temp = head;
-    cout << "President: " << temp->PRN << "\t\t" << temp->Name << "\n";
+void SLL::display()
+{
+    if (head == nullptr)
+    {
+        cout << "\n The list is empty.\n";
+        return;
+    }
+
+    node *temp = head;
+    cout << "\nClub Members:\n";
+    cout << temp->PRN << "\t\t" << temp->Name << " (President)\n";
     temp = temp->next;
-    while (temp != last) {
-        cout << "Member: " << temp->PRN << "\t\t" << temp->Name << "\n";
+
+    while (temp != last)
+    {
+        cout << temp->PRN << "\t\t" << temp->Name << " (Member)\n";
         temp = temp->next;
     }
-    cout << "Secretary: " << temp->PRN << "\t\t" << temp->Name << "\n";
+
+    if (temp != nullptr)
+    {
+        cout << temp->PRN << "\t\t" << temp->Name << " (Secretary)\n";
+    }
+    cout<<"Total members :"<<count<<endl;
 }
 
-int main() {
-    SLL S1;
-    S1.create();
-    S1.display();
+void SLL::search()
+{
+    if (head == nullptr)
+    {
+        cout << "\n The list is empty. Cannot search.\n";
+        return;
+    }
+
+    int prn_key;
+    cout << "\n Enter the PRN number to search: ";
+    cin >> prn_key;
+
+    node *temp = head;
+    while (temp != nullptr)
+    {
+        if (temp->PRN == prn_key)
+        {
+            cout << "PRN " << prn_key << " found: " << temp->Name << "\n";
+            return;
+        }
+        temp = temp->next;
+    }
+    cout << "PRN " << prn_key << " not found in the list.\n";
+}
+
+int main()
+{
+    SLL S;
+    int choice;
+
+    do
+    {
+        cout << "\n--- Pinnacle Club Menu ---\n";
+        cout << "1. Create List\n";
+        cout << "2. Display List\n";
+        cout << "3. Search Member by PRN\n";
+        cout << "4. Exit\n";
+        cout << "Enter your choice: ";
+        cin >> choice;
+
+        switch (choice)
+        {
+        case 1:
+            S.create();
+            break;
+        case 2:
+            S.display();
+            break;
+        case 3:
+            S.search();
+            break;
+        case 4:
+            cout<<"Exiting program."<<endl;
+            break;
+        default:
+            cout<<"Invalid choice! Please try again."<<endl;
+        }
+    } while (choice != 4);
+
     return 0;
 }
